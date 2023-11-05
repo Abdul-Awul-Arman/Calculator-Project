cd8cd1bcdfa91c52be909fb5545192a61f4836da
arman52114 <129601164+arman52114@users.noreply.github.com>
2023-11-05 21:29:44 +0600

    this time i'm Implement the number and symbol function

--- project.js
+++ project.js
@@ -1,9 +1,30 @@
 
+ let screen="0";
+
+function buttonClick(value) {
+  if(isNaN(parseInt(value))==false)
+  {
+      forNumber(value);
+  }
+  else
+  {
+      forSymbol(value);
+  }
+}
+
+function forNumber(value)
+{
+    
+}
+function forSymbol(value)
+{
+    
+}
+
 function mainFunction() {
-    let allButton = document.querySelector(".allbutton");
-    allButton.addEventListener("click",function (event) {
-
-        
-
+  let allbutton=document.querySelector("#allbutton");
+    allbutton.addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
     });
 }
+mainFunction();
