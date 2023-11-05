04c6d58bfd1558143f8d7bc549a29452d91a6ae4
arman52114 <129601164+arman52114@users.noreply.github.com>
2023-11-05 23:01:04 +0600

    make some change in code add some important funcation and tried  discribe the important part of the of the js code

--- project.js
+++ project.js
@@ -1,6 +1,12 @@
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// Hey this AbdulAwulArman i'm doing this Calculator(clone) Project for enhance my html,css and js knowledge.
+// i tried my best to describe the important portion of this js code.
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
- let screen="0";
-
+ let update="0";
+ 
+ let screen=document.querySelector(".screen");
+ //this function work for manipulate the number and symbol form HTML 
 function buttonClick(value) {
   if(isNaN(parseInt(value))==false)
   {
@@ -10,21 +16,41 @@ function buttonClick(value) {
   {
       forSymbol(value);
   }
+
+  //we put this function here because if any one click the any button that mean's he or she want to see the number in screen
+  updateScreen()
 }
 
+//in this function we work with numbers
 function forNumber(value)
 {
-    
+    if(update === "0")
+    {
+        update=value;
+    }
+    else
+    {
+        update+=value;
+    }
 }
 function forSymbol(value)
 {
     
 }
 
+//mainFunction for grab the all number and symbol button
 function mainFunction() {
   let allbutton=document.querySelector("#allbutton");
     allbutton.addEventListener("click", function (event) {
       buttonClick(event.target.innerText);
     });
 }
+
+//updateScreen work for update number in calculator screen
+function updateScreen()
+{
+    screen.innerText=update;
+}
+
+//in this portion i'm call the mainFunction
 mainFunction();
